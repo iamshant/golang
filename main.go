6fd91aa21d5235812aa33f8d4f141982165ec4ce
iamshant <jakao1234@gmail.com>
2021-01-31 20:43:41 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hello Shant")
+	
+	var string1 string = "My name is Shant"
+	var first_int, second_int int = 10, 20
+	var first_int, second_int int = 10, 20
+	var first_float float64 = 10.2
+
+	fmt.Println(first_int + second_int, first_float)
+	fmt.Println(string1)
+}
